c19f7916c0fa5c2b1b2c64afba620640a37832d8
tugrulakin <tugrulakin@gmail.com>
2026-07-18 16:13:59 +0300

    Refactor Google Sheets connection for Streamlit
    Updated authentication method to use Streamlit secrets and modified print statements for Streamlit compatibility.

--- baglanti.py
+++ baglanti.py
@@ -1,18 +1,20 @@
+import streamlit as st
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # 1. Google'a kimliğimizi (VIP Kartını) gösteriyoruz
 kapsam = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-kimlik = ServiceAccountCredentials.from_json_keyfile_name("sifre.json", kapsam)
+creds_dict = json.loads(st.secrets["GSPREAD_JSON"])
+kimlik = ServiceAccountCredentials.from_json_dict(creds_dict, kapsam)
+# ----------------------------------------
+
 client = gspread.authorize(kimlik)
 
 # 2. Google E-Tablo dosyamızı açıyoruz
-# LÜTFEN DİKKAT: Aşağıdaki tırnak içindeki adresi SİLİN ve 
-# kendi Google E-Tablolar dosyanızın tarayıcıdaki tam linkini yapıştırın.
 tablo_linki = "https://docs.google.com/spreadsheets/d/10Cr8YqLpwMXOglQAPz2fP8gSmZR3EkBnpIU-_XSgClM/edit?gid=0#gid=0"
 tablo = client.open_by_url(tablo_linki).sheet1
 
 # 3. Tablodaki verileri çekip ekrana yazdırıyoruz
+# (Web'de olduğumuz için print yerine st.write kullanmak daha iyidir)
 veriler = tablo.get_all_records()
-print("Bağlantı Başarılı! İşte veri tabanınızdaki ilk ürün:")
-print(veriler[0])
